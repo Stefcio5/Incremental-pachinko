063b422861c621cc5b4aa5fd3b9f65752abbf0c3
Stefcio5 <tefcio5@gmail.com>
2022-10-11 20:16:42 +0200

    Added UI element, changed camera, added physic material to ball

--- Incremental pachinko/Assets/Scripts/Counter.cs
+++ Incremental pachinko/Assets/Scripts/Counter.cs
@@ -8,24 +8,19 @@ public class Counter : MonoBehaviour
 {
     public Text CounterText;
 
-    static int Count = 0;
+    static int Points = 0;
     [SerializeField]
     private int CountMultiplier;
 
     private void Start()
     {
-        Count = 0;
+        Points = 0;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Count += 1 * CountMultiplier;
-        CounterText.text = "Count : " + Count;
+        Points += 1 * CountMultiplier;
+        CounterText.text = "Points : " + Points;
         Destroy(other.gameObject, 2f);
-    }
-    private IEnumerator DestroyBall(GameObject ball)
-    {
-        yield return new WaitForSeconds(2f);
-        Destroy(ball);
     }
 }
